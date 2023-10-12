b76dd69f74c4616728b6643afc2f4a3775110734
sanam453 <sanam.hamza9@gmail.com>
2023-10-12 10:47:35 +0430

    add fontawesome link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Layout>{children}</Layout>
+      <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
       </body>
     </html>
   );
